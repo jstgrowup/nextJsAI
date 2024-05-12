c16a72041667f4be9b43fa81c93bab0706fbe051
jstgrowup <deysubham999@gmail.com>
2024-05-13 00:04:02 +0530

    first commit

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+type ConnectionObject = {
+  isConnected?: number;
+};
+const connection: ConnectionObject = {};
+async function dbConnect(): Promise<void> {
+  if (connection?.isConnected) {
+    console.log("ALready connected to database");
+  }
+  try {
+    const db = await mongoose.connect(process?.env?.MONGODB_URI || "");
+    connection.isConnected = db?.connections[0]?.readyState;
+    console.log("DB connected Successfully");
+  } catch (error) {
+    process.exit(1);
+  }
+}
+export default dbConnect;
