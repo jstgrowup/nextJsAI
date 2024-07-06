88bed534f83972facb5e737ec7ca381f0a015563
jstgrowup <deysubham999@gmail.com>
2024-07-06 21:47:39 +0530

    clearing out for the new next JS

--- src/helpers/sendVerificationEmail.ts
+++
@@ -1,3 +0,0 @@
-import { resend } from "@/lib/resend";
-import VerificationEmail from "../../emails/VerificationEmail";
-console.log("VerificationEmail:", VerificationEmail);
--- src/schemas/messageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const messgaeSchema = z.object({
-  content: z
-    .string()
-    .min(10, { message: "content  must be at least of 10 characters" })
-    .max(300, { message: "Content must be no longer than 300" }),
-});
